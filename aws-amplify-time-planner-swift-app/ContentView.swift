15827d469b0be957590a85fdf01a866038071505
michael lai <michaelhin123@gmail.com>
2023-01-14 22:41:55 +0800

    Initial Commit

---
+++ aws-amplify-time-planner-swift-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aws-amplify-time-planner-swift-app
+//
+//  Created by Michael  Lai on 14/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aws-amplify-time-planner-swift-app/aws_amplify_time_planner_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  aws_amplify_time_planner_swift_appApp.swift
+//  aws-amplify-time-planner-swift-app
+//
+//  Created by Michael  Lai on 14/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct aws_amplify_time_planner_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
